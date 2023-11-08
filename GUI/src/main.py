f443014287f9fcbadaa844caf2c484237e8c0575
WHS <wang8763@naver.com>
2023-11-08 17:18:40 +0900

    UI 수정중~!

--- GUI/src/main.py
+++ GUI/src/main.py
@@ -1,38 +1,36 @@
 # -*- coding: utf-8 -*-
 """
 AFCOM - Serial Communication GUI Program
-Cannot be used directly, it is a part of main.py
+Usage: python main.py
 """
-
-__author__ = 'Mehmet Cagri Aksoy - github.com/mcagriaksoy'
-__annotations__ = 'AFCOM - Serial Communication GUI Program'
+__author__ = "Mehmet Cagri Aksoy - github.com/mcagriaksoy"
+__copyright__ = "Copyright 2023, The AFCOM Project"
+__credits__ = ["Mehmet Cagri Aksoy"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Mehmet Cagri Aksoy"
+__status__ = "Production"
 
 # IMPORTS
 import sys
 import glob
 import os
 
-# Runtime Type Checking
-PROGRAM_TYPE_DEBUG = 1
-PROGRAM_TYPE_RELEASE = 0
+
 
 try:
     import serial
     import serial.tools.list_ports
     from serial import SerialException
-    from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
-    from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+    from PyQt6 import uic
+    from PyQt6.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
+    from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 
-    if (PROGRAM_TYPE_DEBUG):
-        from PyQt5.uic import loadUi
-    else: # PROGRAM_TYPE_RELEASE
-        from ui_config import Ui_main_window
 except ImportError as e:
     print("Import Error! Please install the required libraries: " + str(e))
     sys.exit(1)
 
 # GLOBAL VARIABLES
-SERIAL_INFO = serial.Serial()
 PORTS = []
 
 def get_serial_port():
@@ -70,15 +68,16 @@ class Worker(QObject):
     serial_data = pyqtSignal(str)
 
     @pyqtSlot()
-    def __init__(self):
+    def __init__(self, ser):
         super(Worker, self).__init__()
         self.working = True
+        self.ser = ser
 
     def work(self):
         """ Read data from serial port """
         while self.working:
             try:
-                char = SERIAL_INFO.read()
+                char = self.ser.read()
                 h = char.hex()
                 self.serial_data.emit(h)
             except SerialException:
@@ -87,18 +86,30 @@ class Worker(QObject):
                 self.working = False
             self.finished.emit()
 
-class MainWindow(QMainWindow):
+#UI파일 연결
+#단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
+form_class = uic.loadUiType("ui/main_window.ui")[0]
+
+class MainWindow(QMainWindow, form_class):
     """ Main Window """
     def __init__(self):
         """ Initialize Main Window """
-        QMainWindow.__init__(self)
+        super().__init__()
+
+        # 연결한 Ui를 준비한다.
+        self.setupUi(self)
+        # 화면을 보여준다.
+        self.show()
 
         PORTS = get_serial_port()
 
         self.thread = None
         self.worker = None
+        self.ser = None
+
         self.start_button.clicked.connect(self.start_loop)
-        self.comboBox_3.addItems(PORTS)
+        self.comboBox_Port.addItems(PORTS)
+        self.comboBox_Bit.setCurrentIndex(3)
 
     def print_message_on_screen(self, text):
         """ Print the message on the screen """
@@ -110,19 +121,27 @@ class MainWindow(QMainWindow):
 
     def establish_serial_communication(self):
         """ Establish serial communication """
-        port = self.comboBox_3.currentText()
-        baudrate = self.comboBox_1.currentText()
-        timeout = self.comboBox.currentText()
-        length = self.comboBox_2.currentText()
-        parity = self.comboBox_4.currentText()
-        stopbits = self.comboBox_5.currentText()
-        SERIAL_INFO = serial.Serial(port=str(port),
+        port = self.comboBox_Port.currentText()
+        baudrate = self.comboBox_Baud.currentText()
+        timeout = self.comboBox_TimeOut.currentText()
+        length = self.comboBox_Bit.currentText()
+        parity = self.comboBox_Parity.currentText()
+        stopbits = self.comboBox_StopBit.currentText()
+        self.ser = serial.Serial(port=str(port),
                                     baudrate=int(baudrate, base=10),
                                     timeout=float(timeout),
                                     bytesize=int(length, base=10),
                                     parity = parity[0], #get first character
                                     stopbits = float(stopbits)
                                     )
+        print("start ser : ", self.ser)
+        print("port : " + str(port))
+        print("baudrate : ", int(baudrate, base=10))
+        print("timeout : ", float(timeout))
+        print("bytesize : ", int(length, base=10))
+        print("parity : ", parity[0])
+        print("stopbits : ", float(stopbits))
+        print(self.ser.isOpen())
 
     def start_loop(self):
         """ Start the loop """
@@ -133,7 +152,7 @@ class MainWindow(QMainWindow):
                 "Exception occured while trying establish serial communication!")
 
         try:
-            self.worker = Worker()   # a new worker to perform those tasks
+            self.worker = Worker(self.ser)   # a new worker to perform those tasks
             self.thread = QThread()  # a new thread to run our background tasks in
             # move the worker into the thread, do this first before connecting the signals
             self.worker.moveToThread(self.thread)
@@ -155,29 +174,31 @@ class MainWindow(QMainWindow):
     def stop_loop(self):
         """ Stop the loop """
         self.worker.working = False
+        self.label_PortStatus.setText("NOT CONNECTED!")
+        self.label_PortStatus.setStyleSheet('color: red')
         self.textEdit.setText('Stopped!')
 
     def read_data_from_thread(self, serial_data):
         """ Write the result to the text edit box"""
-        # self.textEdit_3.append("{}".format(i))
+        # self.textEdit_BOX.append("{}".format(i))
         if "ERROR_SERIAL_EXCEPTION" in serial_data:
             self.print_message_on_screen("Serial Exception! Please check the serial port")
-            self.label_5.setText("NOT CONNECTED!")
-            self.label_5.setStyleSheet('color: red')
+            self.label_PortStatus.setText("NOT CONNECTED!")
+            self.label_PortStatus.setStyleSheet('color: red')
         else:
-            self.comboBox.setEnabled(False)
-            self.comboBox_1.setEnabled(False)
-            self.comboBox_2.setEnabled(False)
-            self.comboBox_3.setEnabled(False)
-            self.comboBox_4.setEnabled(False)
-            self.comboBox_5.setEnabled(False)
+            self.comboBox_TimeOut.setEnabled(False)
+            self.comboBox_Baud.setEnabled(False)
+            self.comboBox_Bit.setEnabled(False)
+            self.comboBox_Port.setEnabled(False)
+            self.comboBox_Parity.setEnabled(False)
+            self.comboBox_StopBit.setEnabled(False)
             self.save_button.setEnabled(False)
             self.start_button.setEnabled(False)
 
             self.textEdit.setText('Data Gathering...')
-            self.label_5.setText("CONNECTED!")
-            self.label_5.setStyleSheet('color: green')
-            self.textEdit_3.insertPlainText("{}".format(serial_data))
+            self.label_PortStatus.setText("CONNECTED!")
+            self.label_PortStatus.setStyleSheet('color: green')
+            self.textEdit_BOX.insertPlainText("{}".format(serial_data))
 
     # Save the settings
     def on_save_button_clicked(self):
@@ -190,42 +211,32 @@ class MainWindow(QMainWindow):
     def on_save_txt_button_clicked(self):
         """ Save the values to the TXT file"""
         with open('Output.txt', 'w', encoding='utf-8') as f:
-            my_text = self.textEdit_3.toPlainText()
+            my_text = self.textEdit_BOX.toPlainText()
             f.write(my_text)
             f.close()
 
     def on_end_button_clicked(self):
         """ Stop the process """
         self.textEdit.setText('Stopped!')
-        self.comboBox.setEnabled(True)
-        self.comboBox_1.setEnabled(True)
-        self.comboBox_2.setEnabled(True)
-        self.comboBox_3.setEnabled(True)
-        self.comboBox_4.setEnabled(True)
-        self.comboBox_5.setEnabled(True)
+        self.comboBox_TimeOut.setEnabled(True)
+        self.comboBox_Baud.setEnabled(True)
+        self.comboBox_Bit.setEnabled(True)
+        self.comboBox_Port.setEnabled(True)
+        self.comboBox_Parity.setEnabled(True)
+        self.comboBox_StopBit.setEnabled(True)
         self.save_button.setEnabled(True)
         self.start_button.setEnabled(True)
 
     def on_send_data_button_clicked(self):
         """ Send data to serial port """
-        mytext = self.textEdit_2.toPlainText()
+        mytext = self.textEdit_Send.toPlainText()
         print(mytext.encode())
-        SERIAL_INFO.write(mytext.encode())
+        ser.write(mytext.encode())
 
-def start_ui_design():
+if __name__ == "__main__":
     """ Start the UI Design """
     app = QApplication(sys.argv)
-    window_object = QMainWindow()
-
-    if PROGRAM_TYPE_RELEASE:
-        ui = Ui_main_window()
-        ui.setupUi(window_object)
-    elif PROGRAM_TYPE_DEBUG:
-        file_path = os.path.join("../ui/main_window.ui")
-        if not os.path.exists(file_path):
-            print("UI File Not Found!")
-            sys.exit(1)
-        loadUi(file_path, window_object)
+    window_object = MainWindow()
 
     window_object.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
